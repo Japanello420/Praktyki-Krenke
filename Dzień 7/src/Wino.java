b7c55c99f50f53744653f96d6e2757485e4f101a
Japanello420 <79522144+Japanello420@users.noreply.github.com>
2021-03-09 11:18:32 +0100

    Dzień 7

---
+++ Dzień 7/src/Wino.java
@@ -0,0 +1,10 @@
+abstract public class Wino {
+    abstract void bulgocze();
+    abstract void nalewanie();
+    void jestpite(){
+        System.out.println("wiadomo że będzie pite");
+    }
+    void jestkorkowanie(){
+        System.out.println("Korkowanie");
+    }
+}
---
+++ Dzień 7/src/podajRocznikWina.java
@@ -0,0 +1,16 @@
+public class podajRocznikWina extends Wino {
+    void rocznik(){
+        System.out.println("Rocznik nie znany ale klepie");
+    }
+
+    @Override
+    void bulgocze() {
+        System.out.println("Bul bul bul");
+    }
+
+    @Override
+    void nalewanie() {
+        System.out.println("nalewanie");
+
+    }
+}
